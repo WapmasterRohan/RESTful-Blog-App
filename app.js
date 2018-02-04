916c04f78275e1831e6fbe7111d3df5c33da0ac8
Rohan Gayen <rohangayen@gmail.com>
2018-02-04 21:08:38 +0530

    All files created

--- app.js
+++ app.js
@@ -11,9 +11,10 @@ app.use(bodyParser.urlencoded({
 // app.use(expressSanitizer());
 // app.use(methodOverride('_method')) // Override with POST having ?_method=DELETE
 app.set("view engine", "ejs");
+app.use(express.static("public"));   // to use this folder for stylesheets
 mongoose.connect('mongodb://localhost/blogs');
 
-var db = mongoose.connection;
+// var db = mongoose.connection;
 
 const Schema = mongoose.Schema;
 const blogSchema = new Schema({
@@ -29,27 +30,47 @@ const blogSchema = new Schema({
     // }
 });
 const Blog = mongoose.model('Blog', blogSchema); // singular form of the database name
-// let firstPost = {
-//     title: "First blog post",
-//     body: "My first blog post"
-// };
-// Blog.create(firstPost, function(err, blog) {
-//     if (err) {
-//         console.log("Error occurred");
-//     } else {
-//         console.log(blog);
-//     }
-// });
 
 // Nothing to show at the index route, so redirect it to index route
 app.get("/", (req, res) => res.redirect("/blogs"));
 
+// Index route
 app.get("/blogs", (req, res) => {
     Blog.find({}, function(err, blogs) {
         if (! err) {
-            console.log(blogs);
+            res.render("index", {
+                blogs: blogs,
+                title: "Blog App"
+            })
+        } else {
+            res.render("error");
         }
     });
 })
 
+// New route
+app.get("/blogs/new", (req, res) => res.render("new", {
+    title: "Create Post"
+}));
+
+// Create route
+app.post("/blogs", (req, res) => {
+    let post = req.body;
+    Blog.create(post, function(err, post) {
+        if (err) {
+            console.log("Error occurred");
+            res.render("new", {
+                title: "Create post"
+            });
+        } else {
+            console.log(post);
+            res.render("blogs", {
+                post: post,
+                title: "Blog App"
+            });
+        }
+    });
+});
+
+// Create the server
 app.listen(3000, () => console.log("App is opened in port 3000! "));
